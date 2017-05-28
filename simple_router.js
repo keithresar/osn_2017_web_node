5d88e2abb1d0fe2a9d6009b77eae39c34040d8f1
Keith Resar <keith.resar@422long.com>
2017-05-28 08:25:31 -0500

    -

--- simple_router.js
+++ simple_router.js
@@ -1,14 +1,30 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const request = require('request');
+const app = express();
+
+hit_count = 0;
 
 app.get('/', function (req, res) {
-  status = { 'web': 'ok', 'web_hostname': process.env.HOSTNAME };
+    hit_count += 1;
+    status = { 'web': { 'status': 'ok', 'hit_count': hit_count,'hostname': process.env.HOSTNAME } };
 
-  res.send(JSON.stringify(status, null, 3));
+    // Make API call if routing defined in env
+    if (process.env.API_SERVICE_PORT)  {
+        request.get('http://'+process.env.API_SERVICE_HOST+':'+process.env.API_SERVICE_PORT,
+            function(err,r,body) {
+                status['api'] = JSON.parse(body);
+                res.send(JSON.stringify(status, null, 3));
+            });
+    }  else  {
+        res.send(JSON.stringify(status, null, 3));
+    }
+
 })
 
 app.listen(8080, function () {
   console.log('Listening on port 8080!');
 })
+
+
 
